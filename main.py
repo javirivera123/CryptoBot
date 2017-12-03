d0f127c47943e4a40461218465ba35f1a6e0daaf
Paul Bouquet <paul.bouquet@gmail.com>
2017-12-03 20:28:41 +0100

    Begin telegram handler

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from bittrex import Bittrex, API_V2_0
-
 from config.config import load_config
-
+import telebot
 
 def main():
     config = load_config()
 
-    my_bittrex = Bittrex(config["bittrex-key"]["key"], config["bittrex-key"]["secret"], api_version=API_V2_0)
+    bot = telebot.TeleBot(config["crypto-bot"]["token"])
 
-    print(my_bittrex.get_balance('BTC'))
+    print(bot.get_updates(timeout=0.01))
 
+    # my_bittrex = Bittrex(config["bittrex-key"]["key"], config["bittrex-key"]["secret"], api_version=API_V2_0)
+    #
+    # print(my_bittrex.get_balance('BTC'))
+
+    @bot.message_handler(commands=['herklos'])
+    def send_something(message):
+        bot.reply_to(message, "TEST--")
+
+    #bot.polling()
 
 main()
