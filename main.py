572939f08bc132c61164358bb21436a7e7f95ba4
Paul Bouquet <paul.bouquet@gmail.com>
2017-12-03 19:50:54 +0100

    First main

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from bittrex import Bittrex
+from bittrex import Bittrex, API_V2_0
+
+from config.config import load_config
+
 
 def main():
     config = load_config()
 
-    my_bittrex = Bittrex(config["bittrex-key"]["key"], config["bittrex-key"]["secret"], api_version="API_V2_0")
+    my_bittrex = Bittrex(config["bittrex-key"]["key"], config["bittrex-key"]["secret"], api_version=API_V2_0)
 
-    my_bittrex.get_balance('BTC')
+    print(my_bittrex.get_balance('BTC'))
+
+
+main()
