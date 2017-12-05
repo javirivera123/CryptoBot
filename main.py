f5f2a15bd3756cd12a930a04fceebf6f27418123
Paul Bouquet <paul.bouquet@gmail.com>
2017-12-05 19:58:01 +0100

    Try telethon

--- main.py
+++ main.py
@@ -1,21 +1,64 @@
+from time import sleep
+
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+
 from config.config import load_config
 import telebot
+from telethon import TelegramClient
 
-def main():
-    config = load_config()
+config = load_config()
+dialogs = []
+users = []
+chats = []
 
-    bot = telebot.TeleBot(config["crypto-bot"]["token"])
+last_date = None
+chunk_size = 20
 
-    print(bot.get_updates(timeout=0.01))
+client = TelegramClient('some_name', config["telegram-api"]["api_id"], config["telegram-api"]["api_hash"])
+client.connect()
+client.is_user_authorized()
+client.send_code_request(config["telegram-api"]["phone_number"])
+myself = client.sign_in(config["telegram-api"]["phone_number"], input('Enter code: '))
+
+channel = client.get_entity('https://t.me/dlavrov_tch')
+
+
+while True:
+    result = client(GetDialogsRequest(
+                 offset_date=last_date,
+                 offset_id=0,
+                 offset_peer=InputPeerEmpty(),
+                 limit=chunk_size
+             ))
+    dialogs.extend(result.dialogs)
+    users.extend(result.users)
+    chats.extend(result.chats)
+    if not result.messages:
+        break
+    last_date = min(msg.date for msg in result.messages)
+    sleep(2)
+
+print(result)
+
+# def main():
+
+
+    # bot = telebot.TeleBot(config["crypto-bot"]["token"])
+    #
+    # print(bot.get_updates(timeout=0.01))
+
+
 
     # my_bittrex = Bittrex(config["bittrex-key"]["key"], config["bittrex-key"]["secret"], api_version=API_V2_0)
     #
     # print(my_bittrex.get_balance('BTC'))
 
-    @bot.message_handler(commands=['herklos'])
-    def send_something(message):
-        bot.reply_to(message, "TEST--")
+    # @bot.message_handler(commands=['herklos'])
+    # def send_something(message):
+    #     bot.reply_to(message, "TEST--")
+    #
+    # bot.polling()
 
-    #bot.polling()
 
-main()
+# main()
