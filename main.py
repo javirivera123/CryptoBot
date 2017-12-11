45f9ac3283407cd5689c06ee5fd98db52fb896d3
Paul Bouquet <paul.bouquet@gmail.com>
2017-12-11 14:51:52 +0100

    Update architecture

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import logging
 from logging.config import fileConfig
-from telethon import TelegramClient
 from telethon.tl.types import UpdateNewChannelMessage
-
 from config.config import CryptoBotConfig
 from patterns.pattern_parser import PatternParser
+from telegram.telegram_driver import TelegramDriver
 
 # General
+
 config = CryptoBotConfig()
 user_config = config.get_user_config()
 pattern_config = config.get_pattern_config()
@@ -23,37 +23,17 @@ def callback(update):
 
         logger.info(update)
         logger.info("Buy : " + str(pattern_parser.get_buy_value()) + " Sell : " + str(pattern_parser.get_sell_value())
-        + " Stop : " + str(pattern_parser.get_stop_value()) + " Target : " + str(pattern_parser.get_target_value()))
+                    + " Stop : " + str(pattern_parser.get_stop_value()) + " Target : " + str(
+            pattern_parser.get_target_value()))
         # logger.info("Message : " + str(result.message))
 
 
-def first_connection(client, config):
-    client.send_code_request(config["telegram-api"]["phone_number"])
-    myself = client.sign_in(config["telegram-api"]["phone_number"], input('Enter code: '))
-    logger.info(myself.stringify())
-
-
 def main():
-    # my_bittrex = Bittrex(config["bittrex-key"]["key"], config["bittrex-key"]["secret"], api_version=API_V2_0)
-    #
-    # print(my_bittrex.get_balance('BTC'))
-
-    client = TelegramClient("Test", user_config["telegram-api"]["api_id"], user_config["telegram-api"]["api_hash"],
-                            update_workers=1, spawn_read_thread=False)
-
-    #
-    client.connect()
-    if client.is_connected():
-
-        if not client.is_user_authorized():
-            first_connection(client, user_config)
-
-            logger.info("Client connected to Telegram.")
-        client.add_update_handler(callback)
-        client.idle()
-        client.disconnect()
-    else:
-        logger.error("Client not connected")
+    td = TelegramDriver(config)
+    td.connect()
+    td.add_handler_update(callback)
+    td.call_idle()
+    td.disconnect()
 
 
 if __name__ == '__main__':
---
+++ telegram/telegram_driver.py
@@ -0,0 +1,39 @@
+import logging
+
+from telethon import TelegramClient
+
+
+class TelegramDriver:
+    def __init__(self, config):
+        self.config = config
+        self.client = None
+        self.logger = logging.getLogger()
+
+    def first_connection(self):
+        self.client.send_code_request(self.config["telegram-api"]["phone_number"])
+        myself = self.client.sign_in(self.config["telegram-api"]["phone_number"], input('Enter code: '))
+        self.logger.info(myself.stringify())
+
+    def connect(self):
+        self.client = TelegramClient("User", self.config["telegram-api"]["api_id"],
+                                     self.config["telegram-api"]["api_hash"],
+                                     update_workers=1,
+                                     spawn_read_thread=False)
+
+        self.client.connect()
+        if self.client.is_connected():
+
+            if not self.client.is_user_authorized():
+                self.first_connection()
+                self.logger.info("Client connected to Telegram.")
+        else:
+            self.logger.error("Client not connected")
+
+    def add_handler_update(self, callback):
+        self.client.add_update_handler(callback)
+
+    def call_idle(self):
+        self.client.idle()
+
+    def disconnect(self):
+        self.client.disconnect()
