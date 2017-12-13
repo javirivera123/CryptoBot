5e960fd0e80b597004df8eafdd658eddc3a865c7
Paul Bouquet <paul.bouquet@gmail.com>
2017-12-13 22:29:49 +0100

    Update exchanges_driver

--- exchanges/exchanges_driver.py
+++ exchanges/exchanges_driver.py
@@ -7,19 +7,17 @@ class BitfinexExchangeDriver:
     def __init__(self, config):
         self.client = Client()
         self.account = TradeClient(config["bitfinex-key"]["key"], config["bitfinex-key"]["secret"])
-        self.balance = None
+        self.balances = None
 
     def get_balance(self, coin=""):
-        return self
+        self.balances = self.account.balances()
+        return self.balances
 
     def get_currencies(self):
         return self.client.symbols()
 
-    def get_markets(self):
-        return self
-
     def get_open_orders(self, market):
-        return self
+        return self.account.active_orders()
 
     def get_ticker(self, market):
         return self.client.ticker(market)
@@ -27,42 +25,39 @@ class BitfinexExchangeDriver:
     def get_orderbook(self, market, book_type=""):
         return self.client.order_book(market)
 
-    def get_market_history(self, market, count):
-        return self
+    def get_market_history(self, market, count=0):
+        return self.account.history(market, since=count)
 
     def buy_order(self, market, price, quantity, order_type):
-        if order_type == "limit":
-            return self
-        else:
-            return self
+        return self.account.place_order(amount=quantity, price=price, symbol=market, side="buy",
+                                        ord_type="exchange " + order_type)
 
     def stop_loss_order(self, market, price, quantity):
-        return self
+        return self.account.place_order(amount=quantity, price=price, symbol=market, side="sell",
+                                        ord_type="exchange stop")
 
     def sell_order(self, market, price, quantity, order_type):
-        if order_type == "limit":
-            return self
-        else:
-            return self
+        return self.account.place_order(amount=quantity, price=price, symbol=market, side="sell",
+                                        ord_type="exchange " + order_type)
 
     def cancel_order(self, order):
-        return self
+        return self.account.delete_order(order)
 
 
 class BittrexExchangeDriver:
     def __init__(self, config):
         self.account = Bittrex(config["bittrex-key"]["key"], config["bittrex-key"]["secret"], api_version=API_V2_0)
-        self.balance = None
+        self.balances = None
 
     def get_balance(self, coin=""):
-        self.balance = self.account.get_balance(coin)
-        return self.balance
+        if coin == "":
+            return self.account.get_balance(coin)
+        else:
+            self.balances = self.account.get_balances()
+            return self.balances
 
     def get_currencies(self):
         return self.account.get_currencies()
-
-    def get_markets(self):
-        return self.account.get_markets()
 
     def get_open_orders(self, market):
         return self.account.get_open_orders(market)
@@ -84,7 +79,7 @@ class BittrexExchangeDriver:
             return self.account.buy_market(market, quantity, price)
 
     def stop_loss_order(self, market, price, quantity):
-        return self
+        return None
 
     def sell_order(self, market, price, quantity, order_type):
         if order_type == "limit":
